19abda8ff7c34addcb04040a20a4b5a9b1d6b9f6
KAL17002493 <74010320+KAL17002493@users.noreply.github.com>
2023-04-24 15:08:37 +0100

    User can now update their "About" and change their avalability.

--- SAD/Controllers/TeacherController.cs
+++ SAD/Controllers/TeacherController.cs
@@ -22,23 +22,39 @@ namespace SAD.Controllers
             return View();
         }
 
+        //TEACHER PROFILE SECTION
+        public async Task<IActionResult> TeacherProfile()
+        {
+            //Get current user
+            var user = await _userManager.GetUserAsync(User);
+
+            //Check if current user exists
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //Pass the user model to view
+            return View(user);
+        }
+
+        [HttpPost]
         public async Task<IActionResult> TeacherProfile(CustomUserModel model)
         {
             //Get current user
             var user = await _userManager.GetUserAsync(User);
 
             //Check if current user exists
-            if (user == null) 
+            if (user == null)
             {
                 return NotFound();
             }
-            else 
+            else
             {
-            user.About = model.About;
-            user.Available = model.Available;
+                user.About = model.About;
+                user.Available = !user.Available;
+                await _userManager.UpdateAsync(user);
             }
-
-            //UserManager.Update(user);
 
             //Pass the user model to view
             return View(user);
