f264875ea61845413d533da2998e6d4fa4856777
KAL17002493 <74010320+KAL17002493@users.noreply.github.com>
2024-06-03 21:19:30 +0100

    Users can autologin by clicking on either of the cards displayed on home page

--- SAD/Controllers/HomeController.cs
+++ SAD/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SAD.Models;
 using System.Diagnostics;
 
@@ -6,15 +7,65 @@ namespace SAD.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<CustomUserModel> _userManager;
+        private readonly SignInManager<CustomUserModel> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UserManager<CustomUserModel> userManager, SignInManager<CustomUserModel> signInManager)
         {
-            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public IActionResult Index()
+        //Autlogin student user
+        [HttpGet]
+        public async Task<IActionResult> AutoLoginStudent()
         {
+            var user = await _userManager.FindByEmailAsync("student@student.com");
+            if (user != null)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("TeacherScreen", "Student");
+            }
+
+            return RedirectToAction("Login", "Account", "Identity");
+        }
+
+        //Autologin tutor user
+        [HttpGet]
+        public async Task<IActionResult> AutoLoginTutor()
+        {
+            var user = await _userManager.FindByEmailAsync("tutor@tutor.com");
+            if (user != null)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("TeacherProfile", "Teacher");
+            }
+
+            return RedirectToAction("Login", "Account", "Identity");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                //Check user role and redirect to page depending on it
+                var user = await _userManager.GetUserAsync(User);
+                if (await _userManager.IsInRoleAsync(user, "Student"))
+                {
+                    return RedirectToAction("TeacherScreen", "Student");
+                }
+                else if (await _userManager.IsInRoleAsync(user, "Tutor"))
+                {
+                    return RedirectToAction("TeacherProfile", "Teacher");
+                }
+                else
+                {
+                    // Handle other roles or unexpected scenarios
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+
             return View();
         }
 
